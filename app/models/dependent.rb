75b1d0264b046472b5a75f6fb15ef8be66ba9dff
Christina Chatham <cchatham@gmail.com>
2020-09-26 15:14:05 -0400

    Get server running, saving employees and dependents

--- app/models/dependent.rb
+++ app/models/dependent.rb
@@ -1,6 +1,6 @@
 class Dependent < ApplicationRecord
 
-  DEPENDENT_DEDUCTION = 500.freeze
+  DEDUCTION = 500.freeze
 
   # The table also check this. (See schema.) Doesn't cost much to double check.
   validates :name, presence: true 
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,4 +7,16 @@ class Employee < ApplicationRecord
   validates :name, presence: true 
 
   has_many :dependents
+
+  # Allow dependents creation at the same time as the employee creation.
+  accepts_nested_attributes_for :dependents, :reject_if => proc { |att| att[:name].blank? }
+
+  def adjusted_salary
+    @adjusted_salary ||= YEARLY_BASE_SALARY - cost
+  end
+
+  def cost
+    #TODO dont run a count query for every employee. Preload the count somehow.
+    @cost ||= DEDUCTION + (dependents.count * Dependent::DEDUCTION)
+  end
 end
