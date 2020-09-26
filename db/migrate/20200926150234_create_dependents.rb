d85a93adc67c01046865996f483a63d9e40e1ee6
Christina Chatham <cchatham@gmail.com>
2020-09-26 11:28:35 -0400

    fix migration

--- db/migrate/20200926150234_create_dependents.rb
+++ db/migrate/20200926150234_create_dependents.rb
@@ -2,12 +2,9 @@ class CreateDependents < ActiveRecord::Migration[6.0]
   def change
     create_table :dependents do |t|
       t.string :name, null: false
-      
+      t.belongs_to :employee, foreign_key: true
+
       t.timestamps
     end
-
-    # This assumes that the employees table exists 
-    # which generally is safe if the migrations are run in order.
-    add_foreign_key :dependents, :employees
   end
 end
