2e0041aabe8cc14da26cc56b988121510f1a68eb
Christina Chatham <cchatham@gmail.com>
2020-09-26 16:27:28 -0400

    implement discount

--- app/models/dependent.rb
+++ app/models/dependent.rb
@@ -1,9 +1,15 @@
 class Dependent < ApplicationRecord
 
   DEDUCTION = 500.freeze
+  DISCOUNT_START_WITH = "a"
 
   # The table also check this. (See schema.) Doesn't cost much to double check.
   validates :name, presence: true 
 
   belongs_to :employee, counter_cache: true
+
+  #TODO database side instead?
+  def discount_eligible?
+    name.downcase.start_with?(DISCOUNT_START_WITH)
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,7 @@ class Employee < ApplicationRecord
 
   YEARLY_BASE_SALARY = 52000.freeze
   DEDUCTION = 1000.freeze
+  DISCOUNT_START_WITH = "a"
 
   # The table also check this. (See schema.) Doesn't cost much to double check.
   validates :name, presence: true 
@@ -16,7 +17,30 @@ class Employee < ApplicationRecord
   end
 
   def cost
-    #TODO dont run a count query for every employee. Preload the count somehow.
-    @cost ||= DEDUCTION + (dependents_count * Dependent::DEDUCTION)
+    @cost ||= employee_deduction + dependents_deduction
+  end
+
+  def discount_eligible?
+    name.downcase.start_with?(DISCOUNT_START_WITH)
+  end
+
+  private
+
+  def apply_discount(number)
+    number - (0.10 * number).floor.to_i
+  end
+
+  def employee_deduction
+    return DEDUCTION unless discount_eligible?
+
+    apply_discount(DEDUCTION)
+  end
+
+  def dependents_deduction
+    #TODO database this?
+    dependents_eligible_count = dependents.select { |d| d.discount_eligible? }.count
+
+    return (dependents_eligible_count * apply_discount(Dependent::DEDUCTION)) 
+      + ((dependents_count - dependents_eligible_count) * Dependent::DEDUCTION)
   end
 end
